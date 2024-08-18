8eb8adac0ad8eccad8fc3f3b312e26c9b00613c2
Ivan Malko <ioannmalko@gmail.com>
2024-08-19 01:57:11 +0300

    add calc and logic

--- Homework1/GitFlowDemo/GitFlowDemo/Program.cs
+++ Homework1/GitFlowDemo/GitFlowDemo/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using GitFlowDemo;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("How are you?");
+Console.WriteLine(Calc.Sum(2, 5));
 Console.ReadLine();
 throw new NotImplementedException("Where is my logic");
