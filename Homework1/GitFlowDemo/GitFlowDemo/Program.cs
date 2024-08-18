f839b1498857078c0057de9d5df06f0b39849dd7
Ivan Malko <ioannmalko@gmail.com>
2024-08-19 01:57:11 +0300

    add calc and logic

--- Homework1/GitFlowDemo/GitFlowDemo/Program.cs
+++ Homework1/GitFlowDemo/GitFlowDemo/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using GitFlowDemo;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("How are you?");
+Console.WriteLine(Calc.Sum(2, 5));
 Console.ReadLine();
