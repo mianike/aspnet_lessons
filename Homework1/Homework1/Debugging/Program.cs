dff2e5691ecb59df4877b703e386a109adc2118d
Ivan Malko <ioannmalko@gmail.com>
2024-08-18 23:51:17 +0300

    homework1-debugging JsonSerializer and add png

---
+++ Homework1/Homework1/Debugging/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+var numbers = new Dictionary<int, string>
+{
+    {1, "one"},
+    {2, "two"},
+    {3, "three"},
+    {4, "four"},
+    {5, "five"}
+};
+
+string json = JsonSerializer.Serialize(numbers);
+Console.WriteLine($"Serialized json: {json}");
+
+
+Console.ReadLine();
