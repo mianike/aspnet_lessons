96599f7e32fcc12db7f5fb3817aa9ce69cba002a
Ivan Malko <ioannmalko@gmail.com>
2024-09-23 18:14:08 +0300

    hw2-a little styling

--- Homework2/MVC/Models/Player.cs
+++ Homework2/MVC/Models/Player.cs
@@ -1,9 +1,13 @@
-﻿namespace ArsenalFans.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArsenalFans.Models
 {
     public class Player
     {
         public int Id { get; set; }
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
         public int Age { get; set; }
         public string Position { get; set; }
