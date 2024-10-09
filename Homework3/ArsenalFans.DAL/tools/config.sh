db84d6c9e5acb5e3a3dca1d026bda889f184f87c
Ivan Malko <ioannmalko@gmail.com>
2024-10-09 19:21:29 +0300

    hw3-remove old data

--- Homework3/ArsenalFans.DAL/tools/config.sh
+++
@@ -1,2 +0,0 @@
-dbContextProject="$thisScriptDirRP/../"
-startupProject="$thisScriptDirRP/../../SynopticumWebAPI"
--- Homework3/ArsenalFans.DAL/tools/migrate.sh
+++
@@ -1,8 +0,0 @@
-﻿#!/bin/bash
-
-thisScriptDir=`dirname $0`
-thisScriptDirRP=`realpath $thisScriptDir`
-
-. $thisScriptDir/config.sh
-
-dotnet ef database update -p "$dbContextProject" -s "$startupProject"
