b3e8b5ddc2f8043c04a8bb9d0acbb0d8d0fa5636
Ivan Malko <ioannmalko@gmail.com>
2024-11-18 19:14:00 +0300

    creation of hw4 based on hw3

---
+++ Homework4/EntityFramework/Services/PostgresPlayerService.cs
@@ -0,0 +1,51 @@
+﻿using ArsenalFans.Services.Contracts;
+using ArsenalFans.Models;
+using ArsenalFans.DAL.Repositories.Contracts;
+
+namespace ArsenalFans.Services
+{
+    public class PostgresPlayerService : IPlayerService
+    {
+        private readonly IPlayerRepository _playerRepository;
+
+        public PostgresPlayerService(IPlayerRepository playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
+
+        public async Task<List<Player>> GetPlayersAsync()
+        {
+            return await _playerRepository.GetPlayersAsync();
+        }
+
+        public async Task<Player> GetPlayerByIdAsync(int id)
+        {
+            return await _playerRepository.GetPlayerByIdAsync(id);
+        }
+
+        public async Task AddPlayerAsync(Player player)
+        {
+            if (string.IsNullOrWhiteSpace(player.FirstName) || string.IsNullOrWhiteSpace(player.LastName))
+            {
+                throw new ArgumentException("Player's first and last name is required");
+            }
+
+            await _playerRepository.AddPlayerAsync(player);
+        }
+
+        public async Task UpdatePlayerAsync(Player updatedPlayer)
+        {
+            if (string.IsNullOrWhiteSpace(updatedPlayer.FirstName) || string.IsNullOrWhiteSpace(updatedPlayer.LastName))
+            {
+                throw new ArgumentException("Player's first and last name is required");
+            }
+
+            await _playerRepository.UpdatePlayerAsync(updatedPlayer);
+        }
+
+        public async Task DeletePlayerAsync(int id)
+        {
+            await _playerRepository.DeletePlayerAsync(id);
+        }
+    }
+}
