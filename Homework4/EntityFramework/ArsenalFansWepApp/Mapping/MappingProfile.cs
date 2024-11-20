ebd40652b7df93dbc87edabcde51028773a775c3
Ivan Malko <ioannmalko@gmail.com>
2024-11-21 01:54:10 +0300

    add mapping

---
+++ Homework4/EntityFramework/ArsenalFansWepApp/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using ArsenalFansModel.Dto;
+using ArsenalFansModel.Entities;
+using ArsenalFansWebApp.Models.ViewModels;
+using AutoMapper;
+
+namespace ArsenalFansWebApp.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<PlayerDto, Player>().ReverseMap();
+            CreateMap<PlayerDto, PlayerViewModel>().ReverseMap();
+        }
+    }
+}
