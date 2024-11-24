cc6fccac39d12c11b0264268576dce73344b1515
Ivan Malko <ioannmalko@gmail.com>
2024-11-25 01:19:28 +0300

    add feedback controller, model and view

---
+++ Homework5/Validation/ArsenalFansWebApp/Controllers/FeedbackController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ArsenalFansWebApp.Models;
+using ArsenalFansWebApp.Validation;
+
+namespace ArsenalFansWebApp.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FeedbackViewModel feedbackViewModel)
+        {
+            var validator = new FeedbackViewModelValidator();
+            var validationResult = validator.Validate(feedbackViewModel);
+
+            if (!validationResult.IsValid)
+            {
+                foreach (var error in validationResult.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+
+                return View(feedbackViewModel);
+            }
+
+            //Code for working with feedback. For example, data mapping and _feedbackService.Send(feedbackDto)
+
+            TempData["SuccessMessage"] = "Feedback submitted successfully!";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Homework5/Validation/ArsenalFansWebApp/Models/FeedbackViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ArsenalFansWebApp.Models
+{
+    public class FeedbackViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
