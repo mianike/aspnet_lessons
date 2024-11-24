79a16da38b0875b5955473ff16bd302a1e93f975
Ivan Malko <ioannmalko@gmail.com>
2024-11-25 01:55:07 +0300

    creating your own attribute to validate a player’s football position

---
+++ Homework5/Validation/ArsenalFansWebApp/Validation/FootballPositionAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArsenalFansWebApp.Validation
+{
+    public class FootballPositionAttribute : ValidationAttribute
+    {
+        private readonly string[] _existingPositions = new[]
+        {
+            "Goalkeeper",
+            "Defender",
+            "Midfielder",
+            "Forward"
+        };
+
+        public override bool IsValid(object? value)
+        {
+            if (value != null && value is string position)
+            {
+                return _existingPositions.Contains(position, StringComparer.OrdinalIgnoreCase);
+            }
+
+            return false;
+        }
+    }
+}
