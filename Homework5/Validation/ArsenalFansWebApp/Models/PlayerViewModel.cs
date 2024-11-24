3092119b2e5f126807da26f596b0300900bf5865
Ivan Malko <ioannmalko@gmail.com>
2024-11-25 01:55:13 +0300

    added attributes for player validation

--- Homework5/Validation/ArsenalFansWebApp/Models/PlayerViewModel.cs
+++ Homework5/Validation/ArsenalFansWebApp/Models/PlayerViewModel.cs
@@ -1,11 +1,23 @@
-﻿namespace ArsenalFansWebApp.Models
+﻿using ArsenalFansWebApp.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArsenalFansWebApp.Models
 {
     public class PlayerViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "FirstName is required")]
+        [MaxLength(100, ErrorMessage = "The FirstName length must not exceed 100 characters")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required")]
+        [MaxLength(100, ErrorMessage = "The LastName length must not exceed 100 characters")]
         public string LastName { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "Invalid date format")]
         public DateOnly BirthDate { get; set; }
+
         public int Age
         {
             get
@@ -23,7 +35,12 @@
             }
         }
 
+        [Required(ErrorMessage = "Position is required")]
+        [FootballPosition(ErrorMessage = "Invalid football position. Valid positions are Goalkeeper, Defender, Midfielder, Forward")]
         public string Position { get; set; }
+
+        [Required(ErrorMessage = "Nationality is required")]
+        [MaxLength(100, ErrorMessage = "The nationality length must not exceed 100 characters")]
         public string Nationality { get; set; }
     }
 }
