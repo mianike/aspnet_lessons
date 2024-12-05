7a6a5969c775a82f539b97832e180b9df1f7d85b
Ivan Malko <ioannmalko@gmail.com>
2024-12-06 01:30:02 +0300

    add extension for configurated services and initialize pipeline

---
+++ Homework6/Routing/CarSpecificationsAPI/Startup/ApplicationPipeline.cs
@@ -0,0 +1,18 @@
+﻿namespace CarSpecificationsAPI.Startup;
+
+public static class ApplicationPipeline
+{
+    public static void InitializePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+    }
+}
---
+++ Homework6/Routing/CarSpecificationsAPI/Startup/CarSpecificationsAPIModule.cs
@@ -0,0 +1,12 @@
+﻿namespace CarSpecificationsAPI.Startup
+{
+    public static class CarSpecificationsAPIModule
+    {
+        public static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+        }
+    }
+}
