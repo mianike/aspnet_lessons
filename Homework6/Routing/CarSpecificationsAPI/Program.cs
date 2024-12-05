898e6b6f2b740d2e92b987205a577096a171295a
Ivan Malko <ioannmalko@gmail.com>
2024-12-06 01:30:18 +0300

    update Program.cs for new extension

--- Homework6/Routing/CarSpecificationsAPI/Program.cs
+++ Homework6/Routing/CarSpecificationsAPI/Program.cs
@@ -1,3 +1,4 @@
+using CarSpecificationsAPI.Startup;
 
 namespace CarSpecificationsAPI
 {
@@ -7,26 +8,11 @@ namespace CarSpecificationsAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            CarSpecificationsAPIModule.ConfigureServices(builder);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
+            app.InitializePipeline();
 
             app.MapControllers();
 
